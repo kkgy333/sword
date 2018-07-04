adb2196da118cde2d674a14b8918fdd57accd08f
kkgy333 <sagay@vip.qq.com>
2018-07-04 18:32:40 +0800

    Initial commit

---
+++ sword-auth/src/main/java/com/github/kkgy333/sword/auth/SwordAuthApplication.java
@@ -0,0 +1,12 @@
+package com.github.kkgy333.sword.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwordAuthApplication.class, args);
+    }
+}
