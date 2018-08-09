c6cf06d2fb290410e8d779db16dd0c03ce48243e
guyang <sagay@vip.qq.com>
2018-08-09 18:05:57 +0800

    测试代码

---
+++ sword-fabric-sdk/src/main/java/com/github/kkgy333/sword/fabric/sdk/Manager/FabricManager.java
@@ -0,0 +1,20 @@
+package com.github.kkgy333.sword.fabric.sdk.Manager;
+
+import com.github.kkgy333.sword.fabric.sdk.IntermediateOrg;
+import com.github.kkgy333.sword.fabric.sdk.entity.SampleOrg;
+
+public class FabricManager {
+
+    private SampleOrg org;
+    public SampleOrg getOrg(){
+        return org;
+    }
+
+    FabricManager(SampleOrg org) {
+        this.org = org;
+    }
+
+
+
+
+}
