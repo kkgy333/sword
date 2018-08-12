61038dd93e18d40e00191793d8b5db426ef3dc77
kkgy333 <sagay@vip.qq.com>
2018-08-12 22:26:38 +0800

    new fabric

---
+++ sword-fabric-service/src/main/java/com/github/kkgy333/sword/fabric/server/dao/mapper/OrgMapper.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2018. Aberic - aberic@qq.com - All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.kkgy333.sword.fabric.server.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.kkgy333.sword.fabric.server.dao.Org;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+/**
+ * 作者：Aberic on 2018/6/9 13:53
+ * 邮箱：abericyang@gmail.com
+ */
+//@Mapper
+public interface OrgMapper extends BaseMapper<Org> {
+//
+//    @Insert("insert into org (msp_id,tls,league_id,date)" +
+//            "values (#{o.mspId},#{o.tls},#{o.leagueId},#{o.date})")
+//    int add(@Param("o") Org org);
+//
+//    @Update("update org set tls=#{o.tls}, msp_id=#{o.mspId}, league_id=#{o.leagueId}" +
+//            " where id=#{o.id}")
+//    int update(@Param("o") Org org);
+//
+//    @Select("select count(msp_id) from org where league_id=#{id}")
+//    int count(@Param("id") int id);
+//
+//    @Select("select count(msp_id) from org")
+//    int countAll();
+//
+//    @Delete("delete from org where id=#{id}")
+//    int delete(@Param("id") int id);
+//
+//    @Delete("delete from org where league_id=#{leagueId}")
+//    int deleteAll(@Param("leagueId") int leagueId);
+//
+//    @Select("select id,tls,msp_id,league_id,date from org where id=#{id}")
+//    @Results({
+//            @Result(property = "id", column = "id"),
+//            @Result(property = "tls", column = "tls"),
+//            @Result(property = "mspId", column = "msp_id"),
+//            @Result(property = "leagueId", column = "league_id"),
+//            @Result(property = "date", column = "date")
+//    })
+//    Org get(@Param("id") int id);
+//
+//    @Select("select id,tls,msp_id,league_id,date from org where league_id=#{id}")
+//    @Results({
+//            @Result(property = "id", column = "id"),
+//            @Result(property = "tls", column = "tls"),
+//            @Result(property = "mspId", column = "msp_id"),
+//            @Result(property = "leagueId", column = "league_id"),
+//            @Result(property = "date", column = "date")
+//    })
+//    List<Org> list(@Param("id") int id);
+//
+//    @Select("select id,tls,msp_id,league_id,date from org")
+//    @Results({
+//            @Result(property = "id", column = "id"),
+//            @Result(property = "tls", column = "tls"),
+//            @Result(property = "mspId", column = "msp_id"),
+//            @Result(property = "leagueId", column = "league_id"),
+//            @Result(property = "date", column = "date")
+//    })
+//    List<Org> listAll();
+
+}
---
+++ sword-fabric-service/src/main/java/com/github/kkgy333/sword/fabric/server/dao/mapper/PeerMapper.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2018. Aberic - aberic@qq.com - All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.kkgy333.sword.fabric.server.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.kkgy333.sword.fabric.server.dao.Peer;
+import com.github.kkgy333.sword.fabric.server.dao.User;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+/**
+ * 作者：Aberic on 2018/6/9 13:53
+ * 邮箱：abericyang@gmail.com
+ */
+//@Mapper
+public interface PeerMapper extends BaseMapper<Peer> {
+
+//    @Insert("insert into peer (name,location,event_hub_location,server_crt_path,org_id,date) " +
+//            "values (#{p.name},#{p.location},#{p.eventHubLocation},#{p.serverCrtPath},#{p.orgId},#{p.date})")
+//    int add(@Param("p") Peer peer);
+//
+//    @Update("update peer set name=#{p.name}, location=#{p.location}" +
+//            ", event_hub_location=#{p.eventHubLocation}" +
+//            ", server_crt_path=#{p.serverCrtPath} where id=#{p.id}")
+//    int update(@Param("p") Peer peer);
+//
+//    @Update("update peer set name=#{p.name}, location=#{p.location}" +
+//            ", event_hub_location=#{p.eventHubLocation} where id=#{p.id}")
+//    int updateWithNoFile(@Param("p") Peer peer);
+//
+//    @Select("select count(name) from peer where org_id=#{id}")
+//    int count(@Param("id") int id);
+//
+//    @Select("select count(name) from peer")
+//    int countAll();
+//
+//    @Delete("delete from peer where id=#{id}")
+//    int delete(@Param("id") int id);
+//
+//    @Delete("delete from peer where org_id=#{orgId}")
+//    int deleteAll(@Param("orgId") int orgId);
+//
+//    @Select("select id,name,location,event_hub_location,server_crt_path,org_id,date from peer where id=#{id}")
+//    @Results({
+//            @Result(property = "id", column = "id"),
+//            @Result(property = "name", column = "name"),
+//            @Result(property = "location", column = "location"),
+//            @Result(property = "eventHubLocation", column = "event_hub_location"),
+//            @Result(property = "serverCrtPath", column = "server_crt_path"),
+//            @Result(property = "orgId", column = "org_id"),
+//            @Result(property = "date", column = "date")
+//    })
+//    Peer get(@Param("id") int id);
+//
+//    @Select("select id,name,location,event_hub_location,server_crt_path,org_id,date from peer where org_id=#{id}")
+//    @Results({
+//            @Result(property = "id", column = "id"),
+//            @Result(property = "name", column = "name"),
+//            @Result(property = "location", column = "location"),
+//            @Result(property = "eventHubLocation", column = "event_hub_location"),
+//            @Result(property = "serverCrtPath", column = "server_crt_path"),
+//            @Result(property = "orgId", column = "org_id"),
+//            @Result(property = "date", column = "date")
+//    })
+//    List<Peer> list(@Param("id") int id);
+//
+//    @Select("select id,name,location,event_hub_location,server_crt_path,org_id,date from peer")
+//    @Results({
+//            @Result(property = "id", column = "id"),
+//            @Result(property = "name", column = "name"),
+//            @Result(property = "location", column = "location"),
+//            @Result(property = "eventHubLocation", column = "event_hub_location"),
+//            @Result(property = "serverCrtPath", column = "server_crt_path"),
+//            @Result(property = "orgId", column = "org_id"),
+//            @Result(property = "date", column = "date")
+//    })
+//    List<Peer> listAll();
+
+}
